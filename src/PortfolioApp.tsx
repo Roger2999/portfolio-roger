0647f1640d0ef47d955a358f4b6ea6a5a4656f05
roger2999 <rogergutierrezmartinez900@gmail.com>
2026-01-07 02:27:30 -0500

    refactor: restructure PortfolioApp layout and enhance global styles

--- src/PortfolioApp.tsx
+++ src/PortfolioApp.tsx
@@ -5,13 +5,15 @@ export const PortfolioApp = () => {
   const toggleTheme = useThemeStore((state) => state.toggleTheme);
 
   return (
-    <>
-      <div className="portfolio-container h-dvh w-dvw">
+    <div className="app-container flex flex-col h-dvh w-dvw">
+      <header className="header">header</header>
+      <main className="portfolio-container main flex-1">
         <h1>Portfolio</h1>
         <button className="btn-theme" onClick={toggleTheme}>
           Cambiar a {theme === "dark" ? "Claro" : "Oscuro"}
         </button>
-      </div>
-    </>
+      </main>
+      <footer className="footer">footer</footer>
+    </div>
   );
 };
