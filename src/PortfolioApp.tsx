9fd3a4a4e4d0ba3939435692a0cadf805147c44b
roger2999 <rogergutierrezmartinez900@gmail.com>
2026-01-11 04:30:11 -0500

    Enhance PortfolioApp with profile icon, update ButtonLink styles, and improve layout structure

--- src/PortfolioApp.tsx
+++ src/PortfolioApp.tsx
@@ -12,7 +12,7 @@ import githubIcon from "./assets/icon-github.svg";
 import { useState } from "react";
 import { useThemeStore } from "./stores/themeStore";
 import { useIntersection } from "./hooks/useIntersection";
-
+import profileIcon from "./assets/icon-profile.png";
 export const PortfolioApp = () => {
   const [openMenu, setOpenMenu] = useState<boolean>(false);
   const theme = useThemeStore((state) => state.theme);
@@ -73,7 +73,19 @@ export const PortfolioApp = () => {
         )}
       </aside>
       <main className="portfolio-container main flex-1 pt-32 p-5">
-        <section id="home" className="home flex flex-col gap-5 px-5 m-auto">
+        <section id="home" className="home flex flex-col gap-8 px-5 m-auto">
+          <article className="flex items-center gap-5">
+            <img src={profileIcon} width={80} height={80} alt="profile photo" />
+            <ButtonLink
+              href=""
+              className={`rounded-md h-fit text-sm font-bold ${
+                theme === "dark"
+                  ? "backdrop-blur-md bg-white/30 border-gray-500 hover:bg-white/10 text-gray-200"
+                  : "backdrop-blur-md bg-black/10 border-gray-200 hover:bg-black/70 hover:text-white text-gray-800"
+              } `}
+              label="Disponible"
+            />
+          </article>
           <h1 className="font-bold">¡Hola, soy Roger!</h1>
           <p className="text-justify">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
@@ -88,6 +100,11 @@ export const PortfolioApp = () => {
               href="https://wa.me/+5354849352"
               target="_blank"
               rel="noopener noreferrer"
+              className={`${
+                theme === "dark"
+                  ? "backdrop-blur-md bg-white/30 border-gray-500 hover:bg-white/10 text-gray-200"
+                  : "backdrop-blur-md bg-black/10 border-gray-200 hover:bg-black/70 hover:text-white text-gray-800"
+              } `}
             />
             <ButtonLink
               label="GitHub"
@@ -95,6 +112,11 @@ export const PortfolioApp = () => {
               href="https://github.com/roger2999"
               target="_blank"
               rel="noopener noreferrer"
+              className={`${
+                theme === "dark"
+                  ? "backdrop-blur-md bg-white/30 border-gray-500 hover:bg-white/10 text-gray-200"
+                  : "backdrop-blur-md bg-black/10 border-gray-200 hover:bg-black/70 hover:text-white text-gray-800"
+              } `}
             />
           </article>
         </section>
@@ -106,7 +128,7 @@ export const PortfolioApp = () => {
             molestias veniam perferendis ex blanditiis vel, praesentium velit,
             cupiditate culpa reiciendis odit officiis repudiandae.
           </p>
-          <article className="flex gap-8 flex-wrap">
+          <article className="buttons-container flex gap-8 flex-wrap">
             <ButtonLink
               label="Contactame"
               icon="src/assets/icon-preview-button.png"
--- src/components/ButtonLink/ButtonLink.tsx
+++ src/components/ButtonLink/ButtonLink.tsx
@@ -1,5 +1,3 @@
-import { useThemeStore } from "../../stores/themeStore";
-
 interface ButtonProps extends React.ComponentPropsWithRef<"a"> {
   className?: string;
   label?: string;
@@ -11,23 +9,23 @@ export const ButtonLink = ({
   icon,
   ...props
 }: ButtonProps) => {
-  const theme = useThemeStore((state) => state.theme);
   return (
     <a
       {...props}
-      className={`btn-link flex gap-3 min-w-48 p-1 px-5 rounded-md border ${
-        theme === "dark"
-          ? "backdrop-blur-md bg-white/30 border-gray-500 hover:bg-white/10 text-gray-200"
-          : "backdrop-blur-md bg-black/10 border-gray-200 hover:bg-black/70 hover:text-white text-gray-800"
-      } ${className ? className : ""}`}
+      className={`btn-link flex gap-3 w-48 p-1 px-5 border rounded-2xl ${
+        className || ""
+      }`}
     >
-      <img
-        src={icon}
-        alt="icono de link"
-        loading="lazy"
-        decoding="async"
-        className="flex items-center justify-center w-8 h-8"
-      />
+      {icon && (
+        <img
+          src={icon}
+          alt="icono de link"
+          loading="lazy"
+          decoding="async"
+          className="flex items-center justify-center w-8 h-8"
+        />
+      )}
+
       <span className="flex items-center justify-center">{label}</span>
     </a>
   );
